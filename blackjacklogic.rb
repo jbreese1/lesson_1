3a5ace669e1942c97815617d6ca71eaaf1cd06b7
Jim Breese <studiodjb@gmail.com>
2014-11-16 16:30:59 -0800

    logic22

--- blackjacklogic.rb
+++ blackjacklogic.rb
@@ -1,13 +1,9 @@
 require 'pry'
 
 def add_card_to_total(card_total_array, additional_card)
-  if additional_card.kind_of?(Integer)
-    card_total_array.map! { |value| value + additional_card }
-  else
-    total_1 = card_total_array.flatten.map { |value| value + 1 }
-    total_2 = card_total_array.flatten.map { |value| value + 11 }
-    card_total_array = total_1 + total_2
-  end
+  total_1 = card_total_array.flatten.map { |value| value + 1 }
+  total_2 = card_total_array.flatten.map { |value| value + 11 }
+  card_total_array = total_1 + total_2
 end
 
 player_card_total = [0]
@@ -16,7 +12,6 @@ new_card = [1, 11]
 
 add_card_to_total(player_card_total, new_card)
 binding.pry
-
 
 second = 5
 
